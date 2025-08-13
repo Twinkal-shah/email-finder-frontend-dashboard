8db7c15425e4e16c1db696232e4c8c872ad580e6
twinkal-shah <twinkalshah719@gmail.com>
2025-08-13 23:23:44 +0530

    change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Bricolage Grotesque"', 'ui-sans-serif', 'system-ui', '-apple-system', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 } 
